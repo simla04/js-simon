c8b3518d6ba881e87d6611a408a7934ebf58e35d
simla04 <jessicasowi@gmail.com>
2022-11-04 09:17:23 +0100

    da completare:  con array(raccolta 5 numeri randomici) ed addEventListener

--- assets/main.js
+++ assets/main.js
@@ -11,7 +11,17 @@ Consigli del giorno:
 */
 
 // Visualizzare in pagina 5 numeri casuali.
-let numeroRandom = Math.floor(Math.random() * 5) + 1;
+let numeroRandom = Math.floor(Math.random() * 100) + 1;
 console.log(numeroRandom)
 document.getElementById("titleRandom").innerText =`
-il mio numero random da 1 a 5 è :  ${numeroRandom}`;
+i miei 5 numeri randomici da 1 a 100 sono :  ${numeroRandom}`;
+
+// Da lì(=questo evento precedente) parte un timer di 30 secondi.
+let tempo = 10 * 1000;
+
+//e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente,
+
+
+setTimeout( function(){
+    prompt('ora che i numeri sono scomparsi prova a riscrivere quelli che ti ricordi')
+})
